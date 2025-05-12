9f35f535439f62e079d80023db1fc7b2a7546ef4
chatunity <114757102+chatunitycenter@users.noreply.github.com>
2025-05-12 14:48:55 +0000

    update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const rl = createInterface(process.stdin, process.stdout)
 // Aumenta il limite dei listener
 EventEmitter.defaultMaxListeners = 20;
 
-say('CHATUNITY\nBot\n4.0', {
+say('CHATUNITY\nBot\nessential', {
     font: 'chrome',
     align: 'center',
     gradient: ['red', 'magenta']})
